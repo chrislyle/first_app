e61f46f2674e14d9fa033fd728c9885a1924e07e
root <root@centos-ruby.starnet>
2013-11-27 11:19:43 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '41edfab7ce57a64b728fa81db8ebb4179af89df1a4d7bf0eadc9b69592802040ecb24cbc220f7f841dce1eec0788c46aae076875718b2b1ea498dd4ab306d0e5'
